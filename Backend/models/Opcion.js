b2886644fa6769cef0340fe8535152eba42ed77b
Jesus-Armendariz <92763014+Jesus-Armendariz@users.noreply.github.com>
2025-05-06 11:50:54 -0600

    Update Opcion.js

--- Backend/models/Opcion.js
+++ Backend/models/Opcion.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const opcionSchema = new mongoose.Schema({
-    descripcion: { type: String, required: true }
+    descripcion: { type: String, required: true },
+    idVotacion: {type: mongoose.Schema.Types.ObjectId, ref: 'idVotacion', required: true }
 }, { collection: 'opciones', timestamps: true });
 
 module.exports = mongoose.model('Opcion', opcionSchema);
